1c2103cf1281776ab04f8d142e95f6fdf38abcf8
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-08-11 01:33:09 +0900

    💙 feat: 보안 그룹 생성

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import boto3
 import os
 import subprocess
 import scheme
+from botocore.exceptions import ClientError
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -21,7 +22,37 @@ async def regist_iam_user(req: scheme.IamReq):
     os.system("aws configure set aws_access_key_id " + req.access_key +" --profile " + req.profile)
     os.system("aws configure set aws_secret_access_key " + req.secret_key + " --profile " + req.profile)
     os.system("aws configure set region ap-northeast-2 --profile " + req.profile)
-    
+    return {
+        "message":"created well."
+    }
+
+@app.get("/vpc")
+async def get_vpcs(req: scheme.Ec2Req):
+    os.environ['AWS_PROFILE'] = req.profile
+    client = boto3.client('ec2', region_name = 'ap-northeast-2')
+    response = client.describe_vpcs()
+    vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
+    try:
+        response = client.create_security_group(GroupName='KUBERLOUDY_SECURITY_GROUP',
+                                            Description='KUBERLOUDY_SECURITY_GROUP',
+                                            VpcId=vpc_id)
+        security_group_id = response['GroupId']
+
+        data = client.authorize_security_group_ingress(
+            GroupId=security_group_id,
+            IpPermissions=[
+                {'IpProtocol': 'tcp',
+                'FromPort': 80,
+                'ToPort': 80,
+                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+                {'IpProtocol': 'tcp',
+                'FromPort': 22,
+                'ToPort': 22,
+                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+            ])
+        return data
+    except ClientError as e:
+        return e
     
 @app.post("/ec2/start")
 async def create_standard_ec2(req: scheme.Ec2Req):
